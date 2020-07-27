2e5c72719354ed8e138fc1894273863735a4d0bc
Cameron Cousins <cameroncousins18@gmail.com>
2020-07-27 20:43:56 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 sentence = ["Hello there, and how are you?"]
 
 def reverse_each_word(sentence)
-  sentence_array = sentence.split 
+  sentence_array = sentence.split   #
   sentence_array.collect do |words|
     words.reverse
   end.join(" ")
