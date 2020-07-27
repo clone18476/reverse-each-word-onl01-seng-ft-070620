c18cbbae55baa12ab22c904e1ee398bb146c4092
Cameron Cousins <cameroncousins18@gmail.com>
2020-07-27 20:47:40 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,14 @@
 sentence = ["Hello there, and how are you?"]
 
-def reverse_each_word(sentence)
-  sentence_array = sentence.split   #
-  sentence_array.collect do |words|
-    words.reverse
+def reverse_each_word(sentence) 
+    # defines reverse_each_word method taking in
+    # argument of the array with one string element
+  sentence_array = sentence.split   
+    # sets a new variable sentence_array to equal the sentence array with each word
+    # of the string being it's own array
+  sentence_array.collect do |words|  
+    words.reverse   
+      # collects value of sentence_array which we set to sentence.split
   end.join(" ")
   sentence_array.each do |words|
     "#{words.reverse}" = 
